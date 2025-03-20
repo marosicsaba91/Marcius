90af028e90f6939e5e61585c4afb5ac9a974ef57
Csaba Marosi <marosicsaba91@gmail.com>
2025-03-20 21:07:42 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5;
+
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new(hInput, 0, vInput);
+        direction.Normalize();
+
+        Vector3 velocity = direction * speed;
+
+        transform.position += velocity * Time.deltaTime;
+    }
+}
